4207283080321aad6dce6ac90960ee4099b14afc
Sarath S Menon <sarath.s.menon@sap.com>
2021-10-03 17:13:45 +0530

    fixed html-unescape import issue

--- utils/commons.js
+++ utils/commons.js
@@ -1,9 +1,4 @@
-import decode from 'decode-html'
 
 export function shuffleValues(values) {
     return values?.sort((a, b) => Math.random() - 0.5) || []
-}
-
-export function decodeText(text) {
-    return unescape(decode(text).split('&#039;').join('\''))
 }
