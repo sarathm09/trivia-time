5d08ac94b677e5e25ad19adac224a3d38f88a4b4
Sarath S Menon <sarath.s.menon@sap.com>
2021-10-09 16:09:37 +0530

    bug fixes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { supabase } from '#utils/supabase'
+import { useRouter } from 'next/router'
 
-const index = () => {
+
+import styles from '#styles/Profile.module.css'
+import NewGameInput from '../components/NewGameInput';
+
+export default function Profile() {
+    const [profile, setProfile] = useState(null)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        fetchProfile()
+    }, [])
+
+    async function fetchProfile() {
+        const profileData = await supabase.auth.user()
+        if (!profileData) {
+            router.push('/sign-in')
+        } else {
+            setProfile(profileData)
+        }
+    }
+
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.contentBox}>
+                <div className={styles.newSessionBox}>
+                    <NewGameInput profile={profile} />
+                </div>
+                <div className={styles.scoreBox}>
+                </div>
+            </div>
 
         </div>
     )
 }
-
-export default index
