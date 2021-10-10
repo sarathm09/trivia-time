da03ec078e035a19e442953eca9fd4fe57f0a43e
Sarath S Menon <sarath.s.menon@sap.com>
2021-10-10 22:28:21 +0530

    minor css improvements

--- components/UserSessionsList.js
+++ components/UserSessionsList.js
@@ -15,33 +15,38 @@ const UserSessionsList = ({ userSessions }) => {
         <div className={styles.userSessionsList}>
             <h3>Games Played</h3>
 
-            <table className={styles.userSessionsTable}>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Date</th>
-                        <th>Categories</th>
-                        <th>Difficulty</th>
-                        <th>Score</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {userSessions && userSessions.map((session, i) => (
-                        <Link href="/play/trivia" key={session.session_id}>
-                            <tr key={session.session_id} className={session.active ? styles.activeRow : ''}>
-                                <td>{i + 1}</td>
-                                <td>{new Date(session.created_at).toLocaleString()}</td>
-                                <td>{session.categories.slice(0, 5).join(', ')} {session.categories.length > 5 ? `+${session.categories.length - 5} more` : ''}</td>
-                                <td>{session.difficulties.join(', ')}</td>
-                                <td>{session.score}</td>
-                                <td>{session.active ? 'Ongoing' : 'Completed'}</td>
-                            </tr>
-                        </Link>
-                    ))}
+            {userSessions && userSessions.length ? (
+                <table className={styles.userSessionsTable}>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Date</th>
+                            <th>Categories</th>
+                            <th>Difficulty</th>
+                            <th>Score</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userSessions.map((session, i) => (
+                            <Link href="/play/trivia" key={session.session_id}>
+                                <tr key={session.session_id} className={session.active ? styles.activeRow : ''}>
+                                    <td>{i + 1}</td>
+                                    <td>{new Date(session.created_at).toLocaleString()}</td>
+                                    <td>{session.categories.slice(0, 5).join(', ')} {session.categories.length > 5 ? `+${session.categories.length - 5} more` : ''}</td>
+                                    <td>{session.difficulties.join(', ')}</td>
+                                    <td>{session.score}</td>
+                                    <td>{session.active ? 'Ongoing' : 'Completed'}</td>
+                                </tr>
+                            </Link>
+                        ))}
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            ) : (
+                <p>No game played till now...</p>
+            )}
+
         </div>
     )
 }
